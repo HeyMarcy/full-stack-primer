c8f26ab71d8d7648ab352e278abf4c8164209617
marcy <marcy.montross@gmail.com>
2017-03-17 13:12:20 -0500

    add component and connected to DOM

--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,17 @@
 import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import CheeseList from '../components/cheese-list';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+const bluecheese = [
+    "Bath Blue",
+    "Barkham Blue",
+    "Buxton Blue"
+];
+document.addEventListener('DOMContentLoaded', () =>
+  ReactDOM.render(
+    <CheeseList cheese={bluecheese}/>, document.getElementById('app'))
+);
--- server/index.js
+++ server/index.js
@@ -27,3 +27,27 @@ function runServer() {
 if (require.main === module) {
     runServer();
 }
+
+const cheeses = [
+      "Bath Blue",
+      "Barkham Blue",
+      "Buxton Blue",
+      "Cheshire Blue",
+      "Devon Blue",
+      "Dorset Blue Vinney",
+      "Dovedale",
+      "Exmoor Blue",
+      "Harbourne Blue",
+      "Lanark Blue",
+      "Lymeswold",
+      "Oxford Blue",
+      "Shropshire Blue",
+      "Stichelton",
+      "Stilton",
+      "Blue Wensleydale",
+      "Yorkshire Blue"
+    ];
+
+app.get('/cheeses', (req, res) => {
+   return res.json( cheeses );
+  });
